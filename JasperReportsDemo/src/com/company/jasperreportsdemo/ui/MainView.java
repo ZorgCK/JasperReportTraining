20a7f09dc48fbc205f2c110675bba501f4b1ee4f
Christian <Christian@ZorgMaster.fritz.box>
2016-11-12 22:58:35 +0100

    Report 3 Fertig

--- JasperReportsDemo/src/com/company/jasperreportsdemo/ui/MainView.java
+++ JasperReportsDemo/src/com/company/jasperreportsdemo/ui/MainView.java
@@ -9,8 +9,10 @@ import java.util.Map;
 
 import com.company.jasperreportsdemo.business.ReportCreator;
 import com.company.jasperreportsdemo.dal.EmployeeDAO;
+import com.company.jasperreportsdemo.dal.OrderdetailDAO;
 import com.company.jasperreportsdemo.dal.ProductDAO;
 import com.company.jasperreportsdemo.entities.Employee;
+import com.company.jasperreportsdemo.entities.Orderdetail;
 import com.company.jasperreportsdemo.entities.Product;
 import com.vaadin.shared.ui.MarginInfo;
 import com.vaadin.ui.Alignment;
@@ -86,6 +88,39 @@ public class MainView extends XdevView {
 		report.setDataSource(data);
 		report.setParameterMap(parameterMap);
 		report.setTemplatePath("WebContent/WEB-INF/resources/reports/Report 2 - Group.jasper");
+		
+		try {
+			browserFrame.setSource(report.getResource());
+		} catch (JRException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Event handler delegate method for the {@link XdevButton}
+	 * {@link #button3}.
+	 *
+	 * @see Button.ClickListener#buttonClick(Button.ClickEvent)
+	 * @eventHandlerDelegate Do NOT delete, used by UI designer!
+	 */
+	private void button3_buttonClick(Button.ClickEvent event) {
+		List<Orderdetail> orderDetailList = new OrderdetailDAO().findAll();
+
+		JRBeanCollectionDataSource data = new JRBeanCollectionDataSource(orderDetailList);
+		
+		Map<String, Object> parameterMap = new HashMap<String, Object>();
+		parameterMap.put("creator", "Christian Kümmel");
+		parameterMap.put("created", new Date());
+		
+		ReportCreator report = new ReportCreator();
+		report.setDataSource(data);
+		report.setParameterMap(parameterMap);
+		report.setTemplatePath("WebContent/WEB-INF/resources/reports/Report 3 - Crosstab"
+				+ ".jasper");
 		
 		try {
 			browserFrame.setSource(report.getResource());
@@ -180,6 +215,7 @@ public class MainView extends XdevView {
 	
 		button.addClickListener(event -> this.button_buttonClick(event));
 		button2.addClickListener(event -> this.button2_buttonClick(event));
+		button3.addClickListener(event -> this.button3_buttonClick(event));
 	} // </generated-code>
 
 	// <generated-code name="variables">
